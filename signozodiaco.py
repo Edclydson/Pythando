a5556ea0571ab823527e0667b7e6fb1b3c990674
Edclydson <edclydson.sousa@gmail.com>
2021-11-12 16:50:35 -0300

    COMENTADO

--- signozodiaco.py
+++ signozodiaco.py
@@ -1,23 +1,39 @@
 dia = int(input("Qual o dia do seu aniversário? "))
 mes = int(input("Qual o mês do seu aniversário? "))
 
+#CAPRICORNIO / AQUARIO
 if(mes == 1):
     if(dia <= 20):
         print("Seu signo é Capricornio")
     else:
         print("Seu signo é Aquário")
+#AQUARIO / PEIXES
 elif(mes == 2):
     if(dia <= 19):
         print("Seu signo é Aquário")
     else:
         print("Seu signo é Peixes")
+#PEIXES / ARIES
 elif(mes == 3):
     if(dia <= 20):
         print("Seu signo é Peixes")
     else:
         print("Seu signo é Áries")
+#ARIES / TOURO
 elif(mes == 4):
     if(dia <= 20):
         print("Seu signo é Áries")
     else:
         print("Seu signo é Touro")
+#TOURO / GEMEOS
+elif(mes == 5):
+    if(dia <= 20):
+        print("Seu signo é Touro")
+    else:
+        print("Seu signo é Gemeos")
+#GEMEOS / CANCER
+elif(mes == 6):
+    if(dia <= 20):
+        print("Seu signo é Gemeos")
+    else:
+        print("Seu signo é Cancer")
