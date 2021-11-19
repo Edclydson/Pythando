3772be1cf4b6db44072a8f8bd6bc1bde94700056
Edclydson <edclydson.sousa@gmail.com>
2021-11-19 20:14:54 -0300

    finalizado

--- eleiçao.py
+++ eleiçao.py
@@ -7,45 +7,49 @@ c = 0
 porcnull = 0.0  # variavel p percentual votos nulos
 porcbranco = 0.0  # variavel p percentual votos branco
 tot = 0  # variavel p contar total de votos
+
+
+def voto(p=0):  # função voto
+    esc = candidatos[p]
+    esc[2] = esc[2] + 1
+    print("--------------------")
+    print("     F I M !")
+    print("--------------------")
+    candidatos[p] = esc  # computando voto na variavel candidatos
+    print("\n\n\n\n\n\n\n")
+
+
+def fim():  # função encerra votação
+    print("--------------------")
+    print("     F I M !")
+    print("--------------------")
+    print("\n\n\n")
+    print(" EXIBINDO RESULTADO DA ELEIÇÃO \n")
+    print("COD  |   NOME    |   VOTOS\n")
+    for i in candidatos:  # percorre mostrando candidatos e votos
+        print(*i, "\n", sep="    |    ")
+    porcnull = float((100 * (vtnulo/tot)))  # % de votos nulos
+    porcbranco = float((100 * (vtbranco/tot)))  # % de votos brancos
+    print("Votos branco: ", vtbranco, "/", "%.2f" % porcbranco, "%",
+          "  Votos Nulos: ", vtnulo, "/", "%.2f" % porcnull, "%")
+    print("\n")
+
+
 while(1 > 0):  # loop infinito p votação n acabar
     vt = input("Qual o seu voto: ")
     tot = tot + 1
     for c in candidatos:
-        if (vt == "1"):  # se votar 1
-            esc = candidatos[0]
-            esc[2] = esc[2] + 1
-            print("--------------------")
-            print("     F I M !")
-            print("--------------------")
-            candidatos[0] = esc  # computando voto na variavel candidatos
-            print("\n\n\n\n\n\n\n")
+        if (vt == "1"):
+            voto(p=0)
             break
         elif (vt == "2"):
-            esc = candidatos[1]
-            esc[2] = esc[2] + 1
-            print("--------------------")
-            print("     F I M !")
-            print("--------------------")
-            candidatos[1] = esc
-            print("\n\n\n\n\n\n\n")
+            voto(p=1)
             break
         elif (vt == "3"):
-            esc = candidatos[2]
-            esc[2] = esc[2] + 1
-            print("--------------------")
-            print("     F I M !")
-            print("--------------------")
-            candidatos[2] = esc
-            print("\n\n\n\n\n\n\n")
+            voto(p=2)
             break
         elif (vt == "4"):
-            esc = candidatos[3]
-            esc[2] = esc[2] + 1
-            print("--------------------")
-            print("     F I M !")
-            print("--------------------")
-            candidatos[3] = esc
-            print("\n\n\n\n\n\n\n")
+            voto(p=3)
             break
         elif (vt == "0"):
             vtbranco = vtbranco + 1
@@ -62,17 +66,6 @@ while(1 > 0):  # loop infinito p votação n acabar
             print("\n\n\n\n\n\n\n")
             break
     if (vt == "fim"):
-        print("--------------------")
-        print("     F I M !")
-        print("--------------------")
-        print("\n\n\n")
-        print(" EXIBINDO RESULTADO DA ELEIÇÃO \n")
+        vtnulo = vtnulo - 1
+        fim()
         break
-print("COD  |   NOME    |   VOTOS\n")
-for i in candidatos:
-    print(*i, "\n", sep="    |    ")
-porcnull = float((100 * (vtnulo/tot)))
-porcbranco = float((100 * (vtbranco/tot)))
-print("Votos branco: ", vtbranco, "/", "%.2f" % porcbranco, "%",
-      "  Votos Nulos: ", vtnulo, "/", "%.2f" % porcnull, "%")
-print("\n")
