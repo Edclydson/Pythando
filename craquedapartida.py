7473552358d55f1dedd3413390014567422b72f8
Edclydson <edclydson.sousa@gmail.com>
2021-11-23 17:39:14 -0300

    praticando

--- craquedapartida.py
+++ craquedapartida.py
@@ -1,22 +1,12 @@
-'''
-Faça um programa que simule um lançamento de dados. 
-Lance o dado 100 vezes e armazene os resultados em um vetor. 
-Depois, mostre quantas vezes cada valor foi conseguido. 
-Dica: use um vetor de contadores(1-6) e uma função para gerar numeros aleatórios, 
-simulando os lançamentos dos dados.
-'''
 jogadores = []
-vtnulo = 0  # variavel p contar votos nulos
-vtbranco = 0  # variavel p contar votos brancos
 c = 0
-porcnull = 0.0  # variavel p percentual votos nulos
-porcbranco = 0.0  # variavel p percentual votos branco
 tot = 0  # variavel p contar total de votos
 
-#adicionando os 23 jogadores na lista
-for i in range(1,24):
-    jogadores.append([str(i),0])
-print (jogadores)
+# adicionando os 23 jogadores na lista
+for i in range(1, 24):
+    jogadores.append([str(i), 0])
+print(jogadores)
+
 
 def voto(p=0):  # função voto
     esc = jogadores[p]
@@ -33,22 +23,16 @@ def fim():  # função encerra votação
     print("     F I M !")
     print("-" * 20)
     print("\n\n\n")
-    print(" EXIBINDO RESULTADO DA ELEIÇÃO \n")
+    print(" EXIBINDO RESULTADO DA VOTAÇÃO \n")
     print("Nº JOGADOR  |   VOTOS    |   % VOTOS\n")
     for i in jogadores:  # percorre mostrando candidatos e votos
-        print(*i, "\n", sep="    |    ")
-        #FALTA EXIBIR % DE VOTOS DO JOGADOR
-    porcnull = float((100 * (vtnulo/tot)))  # % de votos nulos
-    porcbranco = float((100 * (vtbranco/tot)))  # % de votos brancos
-    print("Votos branco: ", vtbranco, "/", "%.2f" % porcbranco, "%",
-          "  Votos Nulos: ", vtnulo, "/", "%.2f" % porcnull, "%")
-    print("\n")
+        print(*i, "%.1f" % (float((100 * (i[1]/tot)))), "\n", sep="    |    ")
 
 
 while(1 > 0):  # loop infinito p votação n acabar
     vt = input("Qual o seu voto: ")
     tot = tot + 1
-    for c in jogadores:
+    for c in range(len(jogadores)):
         if (vt == "1"):
             voto(p=0)
             break
@@ -125,4 +109,6 @@ while(1 > 0):  # loop infinito p votação n acabar
             print("Voto inválido!")
             print("\n\n\n\n\n\n\n")
             continue
-    
+    if(vt == '0'):
+        fim()
+        break
