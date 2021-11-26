40460615c15925281d550ac0d24e25a4dccd9c8b
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-26 14:42:50 -0300

    Update baixavideo.py

--- baixavideo.py
+++ baixavideo.py
@@ -1,11 +1,14 @@
+#IMPORTS NECESSARIOS
 from pytube import YouTube
 import pyautogui
 from os import path
+#PEGANDO O PATH
 main = path.join(path.expanduser("~"))
+
 while(True):
     url = pyautogui.prompt("Cole a url aqui:")
     video = YouTube(url)
-    stream = video.streams.get_highest_resolution()
+    stream = video.streams.get_highest_resolution() #BAIXANDO O VIDEO NA MELHOR RESOLUÇAO POSSIVEL
 
-    stream.download(output_path=main+'\\Videos')
+    stream.download(output_path=main+'\\Videos') #SALVANDO NA PASTA VIDEOS DO WINDOWS
     pyautogui.alert("Video baixado!")
