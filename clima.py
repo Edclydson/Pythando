8a93e6dba3d26cc02a3ecdda31527c794056f436
Edclydson <edclydson.sousa@gmail.com>
2021-11-29 17:49:46 -0300

    v1.0

---
+++ clima.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+
+# ID FORTALEZA 8050
+token = "INSIRA O TOKEN AQUI"
+idcidade = '8050'
+
+
+def agorafortaleza():
+
+    url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/" + \
+        idcidade + "/current?token=" + token
+    resposta = requests.request("GET", url)
+    retorno = json.loads(resposta.text)
+    for dados in retorno:
+        if dados == 'id':
+            continue
+        elif dados == 'data':
+            break
+        print(dados + " : " + str(retorno[dados]))
+    for dados in retorno['data']:
+        print(dados+": "+str(retorno['data'][dados]))
+
+
+# CHAMANDO A FUNÇÃO
+agorafortaleza()
